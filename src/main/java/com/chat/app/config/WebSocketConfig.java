bbe50e3207219c90c62518f3682c8d4806aaa496
Yasiralam <yasiralam981@gmail.com>
2025-05-14 15:59:34 +0530

    [🐛]  fixed all the ui and fixed pojo class. lombok error. now chat is working fine

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -18,6 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
--- src/main/java/com/chat/app/model/ChatMessage.java
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -1,13 +1,30 @@
 package com.chat.app.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class ChatMessage {
-
-    private Long id;
     private String sender;
-    private  String content;
+    private String content;
+
+    public ChatMessage() {}
+
+    public ChatMessage(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
+
